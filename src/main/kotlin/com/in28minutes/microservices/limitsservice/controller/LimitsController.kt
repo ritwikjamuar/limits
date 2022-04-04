1db41053eff2217809d5fd9e6ef1eef45809fd08
ritwikjamuar <g!t1-1u6>
2022-04-04 16:10:03 +0530

    In LimitsController.kt, added usage of AppConfiguration.kt to fetch the values of minimum and maximum limits from `application.properties` as opposed to hard-coding it in the source code.

--- src/main/kotlin/com/in28minutes/microservices/limitsservice/controller/LimitsController.kt
+++ src/main/kotlin/com/in28minutes/microservices/limitsservice/controller/LimitsController.kt
@@ -2,6 +2,10 @@ package com.in28minutes.microservices.limitsservice.controller
 
 import com.in28minutes.microservices.limitsservice.bean.Limits
 
+import com.in28minutes.microservices.limitsservice.configuration.AppConfiguration
+
+import org.springframework.beans.factory.annotation.Autowired
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +15,16 @@ import org.springframework.web.bind.annotation.RestController
 @Suppress("unused")
 @RestController
 class LimitsController {
+
+	/*-------------------------------------------------- Components --------------------------------------------------*/
+
+	/**
+	 * Reference of [AppConfiguration] to access the custom fields defined in `application.properties`.
+	 */
+	@Autowired
+	lateinit var configuration: AppConfiguration
+
+	/*------------------------------------------------ Public Methods ------------------------------------------------*/
 
 	/**
 	 * GET Mapping to provide the static value of [Limits].
@@ -22,6 +36,6 @@ class LimitsController {
 			"/limits"
 		]
 	)
-	fun retrieveLimits(): Limits = Limits(1, 1000)
+	fun retrieveLimits(): Limits = Limits(configuration.minimum, configuration.maximum)
 
 }
