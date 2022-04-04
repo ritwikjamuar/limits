b0890899d76c75354e36cfe92911f4574fc51bf2
ritwikjamuar <g!t1-1u6>
2022-04-04 13:39:22 +0530

    Modified LimitsController.kt by adding a simple GET Mapping method that returns a new instance of Limits.kt. Also added documentation.

--- src/main/kotlin/com/in28minutes/microservices/limitsservice/controller/LimitsController.kt
+++ src/main/kotlin/com/in28minutes/microservices/limitsservice/controller/LimitsController.kt
@@ -1,8 +1,27 @@
 package com.in28minutes.microservices.limitsservice.controller
 
+import com.in28minutes.microservices.limitsservice.bean.Limits
+
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * [RestController] for receiving and responding all the HTTP Methods related to [Limits].
+ */
 @Suppress("unused")
 @RestController
 class LimitsController {
+
+	/**
+	 * GET Mapping to provide the static value of [Limits].
+	 *
+	 * @return New instance of [Limits].
+	 */
+	@GetMapping(
+		path = [
+			"/limits"
+		]
+	)
+	fun retrieveLimits(): Limits = Limits(1, 1000)
+
 }
