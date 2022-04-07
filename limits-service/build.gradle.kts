fa417178f91d05e53b27ee6c0a398210f6dbbcdd
ritwikjamuar <g!t1-1u6>
2022-04-08 00:18:39 +0530

    Added module `spring-cloud-config-server` as a dependency of the module `limits-service`.

--- limits-service/build.gradle.kts
+++ limits-service/build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 
+	implementation(project(":spring-cloud-config-server"))
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
