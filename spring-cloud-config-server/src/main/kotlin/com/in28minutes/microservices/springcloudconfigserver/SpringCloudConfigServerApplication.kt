559e45f178dd02e092e1f817c63d15c9d1286517
ritwikjamuar <g!t1-1u6>
2022-04-08 00:36:50 +0530

    Connected the `spring-cloud-config-server` to a local Git Repository for dynamically load the configurations. Also, in SpringCloudConfigServerApplication.kt, added annotation `@EnableConfigServer` which enables this application as the Config-Server.

--- spring-cloud-config-server/src/main/kotlin/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.kt
+++ spring-cloud-config-server/src/main/kotlin/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 
 import org.springframework.boot.runApplication
 
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
 @SpringBootApplication
 class SpringCloudConfigServerApplication
 
