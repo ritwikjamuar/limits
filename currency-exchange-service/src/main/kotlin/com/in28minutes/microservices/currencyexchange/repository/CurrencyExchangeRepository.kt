687756bc25a828582946e7966e93d0197ec73ebf
ritwikjamuar <g!t1-1u6>
2022-04-11 00:57:57 +0530

    In `currency-exchange-service`, under CurrencyExchangeRepository.kt, added a new method that fetches a specific CurrencyExchange.kt based on the values of `from` and `to`.

--- currency-exchange-service/src/main/kotlin/com/in28minutes/microservices/currencyexchange/repository/CurrencyExchangeRepository.kt
+++ currency-exchange-service/src/main/kotlin/com/in28minutes/microservices/currencyexchange/repository/CurrencyExchangeRepository.kt
@@ -7,4 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository
 /**
  * [JpaRepository] of [CurrencyExchange] wth [Long] ID.
  */
-interface CurrencyExchangeRepository : JpaRepository<CurrencyExchange, Long>
+interface CurrencyExchangeRepository : JpaRepository<CurrencyExchange, Long> {
+
+	/**
+	 * Finds a specific [CurrencyExchange] whose [from] and [to] matches.
+	 *
+	 * @param from [String] denoting the ID of currency from which conversion is to take place.
+	 * @param to [String] denoting the ID of currency to which conversion should be computed.
+	 * @return If found, then an instance of [CurrencyExchange], otherwise `null`.
+	 */
+	fun findByFromAndTo(from: String, to: String): CurrencyExchange?
+
+}
