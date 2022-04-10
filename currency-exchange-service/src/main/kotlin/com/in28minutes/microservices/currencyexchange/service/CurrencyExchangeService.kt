f8d8b42d1a2dc455e6c886495c6db3ed431a74ed
ritwikjamuar <g!t1-1u6>
2022-04-11 01:32:58 +0530

    In `currency-exchange-service`, under CurrencyExchangeService.kt, added a method that performs finding the `CurrencyExchange` based on the `from` & `to` values provided.

--- currency-exchange-service/src/main/kotlin/com/in28minutes/microservices/currencyexchange/service/CurrencyExchangeService.kt
+++ currency-exchange-service/src/main/kotlin/com/in28minutes/microservices/currencyexchange/service/CurrencyExchangeService.kt
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchange.service
 
 import com.in28minutes.microservices.currencyexchange.bean.CurrencyExchange
 
+import com.in28minutes.microservices.currencyexchange.exception.CurrencyExchangeNotFoundException
+
 import com.in28minutes.microservices.currencyexchange.repository.CurrencyExchangeRepository
 
 import org.springframework.beans.factory.annotation.Autowired
@@ -23,5 +25,19 @@ class CurrencyExchangeService {
 	private lateinit var repository: CurrencyExchangeRepository
 
 	/*------------------------------------------------ Public Methods ------------------------------------------------*/
+
+	/**
+	 * Finds a specific [CurrencyExchange] whose [from] and [to] matches.
+	 *
+	 * @param from [String] denoting the ID of currency from which conversion is to take place.
+	 * @param to [String] denoting the ID of currency to which conversion should be computed.
+	 * @return An instance of [CurrencyExchange] matching [from] and [to].
+	 * @throws CurrencyExchangeNotFoundException This exception is thrown when an [CurrencyExchange] matching [from]
+	 * and [to] is not found.
+	 */
+	@kotlin.jvm.Throws(CurrencyExchangeNotFoundException::class)
+	fun findByFromAndTo(from: String, to: String): CurrencyExchange =
+		repository.findByFromAndTo(from, to)
+			?: throw CurrencyExchangeNotFoundException("Currency Exchange From: $from -> To: $to : NOT FOUND")
 
 }
