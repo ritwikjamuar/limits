d41d0dabafa7ec6ab7c78c71e9a68a7c0a1ae346
ritwikjamuar <g!t1-1u6>
2022-04-10 22:56:17 +0530

    In `currency-exchange-service`, added CurrencyExchangeController.kt as the REST Controller of the microservice. Also added a GET Mapping to fetch the currency conversion with a dummy value implemented at the time.

---
+++ currency-exchange-service/src/main/kotlin/com/in28minutes/microservices/currencyexchange/controller/CurrencyExchangeController.kt
@@ -0,0 +1,40 @@
+package com.in28minutes.microservices.currencyexchange.controller
+
+import com.in28minutes.microservices.currencyexchange.bean.CurrencyExchange
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+import java.math.BigDecimal
+
+/**
+ * [RestController] for receiving and responding all the HTTP Methods related to [CurrencyExchange].
+ */
+@Suppress("unused")
+@RestController
+class CurrencyExchangeController {
+
+	/*------------------------------------------------ Public Methods ------------------------------------------------*/
+
+	/**
+	 * GET Mapping to provide the value of Currency Exchange.
+	 * TODO : Remove this dummy value return once actual currency conversion is implemented.
+	 *
+	 * @param from [String] denoting the ID of currency from which conversion is to take place.
+	 * @param to [String] denoting the ID of currency to which conversion should be computed.
+	 * @return New instance of [CurrencyExchange].
+	 */
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	fun retrieveExchangeValue(
+		@PathVariable from: String,
+		@PathVariable to: String
+	): CurrencyExchange = CurrencyExchange(
+		1000L,
+		from,
+		to,
+		BigDecimal.ZERO,
+		""
+	)
+
+}
