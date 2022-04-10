eeba4eacc3076216f24bdac8f12c54f157cf2ace
ritwikjamuar <g!t1-1u6>
2022-04-10 23:54:58 +0530

    In `currency-exchange-service`, under build.gradle.kts, added 4 dependencies to the module: 1. Spring Boot Data JPA 2. Spring Boot Validation 3. Spring Doc 4. H2 Database

--- currency-exchange-service/build.gradle.kts
+++ currency-exchange-service/build.gradle.kts
@@ -11,9 +11,14 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
