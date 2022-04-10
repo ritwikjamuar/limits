612c22383ae9ecb756900600f24061cb8d87c0ab
ritwikjamuar <g!t1-1u6>
2022-04-11 00:43:07 +0530

    In `currency-exchange-service`, created data.sql to preload the Data into the Database.

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (1, 'USD', 'INR', 75.92, '');
+
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (2, 'EUR', 'INR', 82.57, '');
+
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (3, 'AUD', 'INR', 56.51, '');
+
+INSERT INTO currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (4, 'GBP', 'INR', 98.89, '');
